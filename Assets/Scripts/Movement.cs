88845c2ae5c26e9608b329bfbdfdc9d23f68ce5a
CBrunoSantos <brunosantos@edu.unifor.br>
2022-04-04 15:52:23 -0300

    tiro

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject croos;
+
+    private float tempoUltimoTiro;
+
+    private bool estaAtirando = false;
+    public Transform croos;
+
+    public GameObject projetil;
+
+    public float velocidadeProjetil;
+
     void Start()
     {
         
@@ -14,7 +23,36 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Atirar();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0.0f);
         transform.position = transform.position + movement * Time.deltaTime * 5;
+    }
+
+        void Atirar(){
+        if (Input.GetMouseButtonDown(0)){
+            Transform shotPoint = croos;
+            
+            GameObject projetils = Instantiate(projetil, shotPoint.position, transform.rotation);
+
+            estaAtirando = true;
+            tempoUltimoTiro = .7f;
+
+
+        if(Input.GetAxis("Horizontal") > 0f || Input.GetAxis("Horizontal") == 0f){
+            projetils.GetComponent<Rigidbody2D>().velocity = new Vector2(velocidadeProjetil, 0);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        else if(Input.GetAxis("Horizontal") < 0f || Input.GetAxis("Horizontal") == 0f){
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            projetils.GetComponent<Rigidbody2D>().velocity = new Vector2(-velocidadeProjetil, 0);
+        }
+        
+        }
+
+
+        tempoUltimoTiro -= Time.deltaTime;
+        if(tempoUltimoTiro <= 0 ){
+            estaAtirando = true;
+        }
     }
 }
---
+++ Assets/Scripts/projetils.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projetils : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int dano;
+    public float tempoDeVida;
+    public float distancia;
+    void Start()
+    {
+        Invoke("DestruirProjetil", tempoDeVida);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.forward, distancia);
+
+        // if(hitInfo.collider != null){
+        //     if(hitInfo.collider.CompareTag("Inimigo")){
+        //         hitInfo.collider.GetComponent<EnemyFollow>().TakeDamage(dano);
+        //     } else if (hitInfo.collider.CompareTag("InimigoPatrulha")){
+        //         hitInfo.collider.GetComponent<SystemRonda>().TakeDamage(dano);
+        //     } else if (hitInfo.collider.CompareTag("InimigoWall")){
+        //         hitInfo.collider.GetComponent<Enemyshot>().TakeDamage(dano);
+        //     }
+        //     DestruirProjetil();
+        // }
+    }
+
+        void DestruirProjetil(){
+        Destroy(gameObject);
+    }
+}
