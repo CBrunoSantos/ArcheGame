fdaf99076ece85d33d7bec5b360ec38056f8c599
Carlos Bruno <49172271+CBrunoSantos@users.noreply.github.com>
2022-08-22 17:03:20 -0300

    barra de vida dos inimigos

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
+    [SerializeField] private HealthBar healthBar;
     public Rigidbody2D rbEnemy;
     public float velEnemy;
     private Transform m_player;
+    float health = 1f;
 
     void Start(){
         m_player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
@@ -17,20 +18,19 @@ public class Enemy : MonoBehaviour
         float _distancia = Vector2.Distance(transform.position,m_player.position);
         transform.position = Vector2.MoveTowards(transform.position,m_player.position,velEnemy * Time.deltaTime);
     }
-
-
-    // void OnCollisionEnter2D(Collision2D collision){
-    //     if(collision.gameObject.tag == "Player"){
-    //         Destroy(gameObject, 0f);
-    //     }
-    // }
-
         void OnCollisionEnter2D(Collision2D collision){
             if(collision.gameObject.tag == "Player"){
                 Destroy(gameObject, 0f);
             }else if(collision.gameObject.tag == "alvo"){
-            Destroy(gameObject);
+            Invoke("tocou", 0f);
         }
     }
+    void tocou(){
+            health -= .5f;
+            healthBar.SetSize(health); 
+                if(health<=0){
+                    Destroy(gameObject, 0f);
+                }
+            }
 
 }
